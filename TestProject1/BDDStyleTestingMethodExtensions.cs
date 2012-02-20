b05a444a423557280451b674db93cf029ff5c029
Jeremy The Super Geek <Jeremy.Hardin@Insperity.com>
2012-02-20 15:48:51 -0600

    Adding the Testable<T> and BDD extension classes to the project.

---
+++ TestProject1/BDDStyleTestingMethodExtensions.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System
+{
+    public static class BDDStyleTestingMethodExtensions
+    {
+
+        /// <summary>
+        /// Tests if the object or value is null.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="error"></param>
+        public static void ShouldBeNull(this Object context, String error = null)
+        {
+            if (error == null) Assert.IsNull(context);
+            else Assert.IsNull(context, error);
+        }
+
+        /// <summary>
+        /// Tests if the object or value is not null.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="error"></param>
+        public static void ShouldNotBeNull(this Object context, String error = null)
+        {
+            if (error == null) Assert.IsNotNull(context);
+            else Assert.IsNotNull(context, error);
+        }
+
+        /// <summary>
+        /// Tests is the object or value equals another.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context"></param>
+        /// <param name="objectToCompare"></param>
+        /// <param name="error"></param>
+        public static void ShouldEqual<T>(this T context, T objectToCompare, String error = null)
+        {
+            if (error == null) Assert.AreEqual<T>(objectToCompare, context);
+            else Assert.AreEqual<T>(objectToCompare, context, error);
+        }
+
+        /// <summary>
+        /// Tests if the object or value does not equal another.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context"></param>
+        /// <param name="objectToCompare"></param>
+        /// <param name="error"></param>
+        public static void ShouldNotEqual<T>(this T context, T objectToCompare, String error = null)
+        {
+            if (error == null) Assert.AreNotEqual<T>(objectToCompare, context);
+            else Assert.AreNotEqual<T>(objectToCompare, context, error);
+        }
+
+        /// <summary>
+        /// Tests if the value is true.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="error"></param>
+        public static void ShouldBeTrue<T>(this T context, String error = null)
+        {
+            var value = Convert.ToBoolean(context);
+            if (error == null) Assert.IsTrue(value);
+            else Assert.IsTrue(value, error);
+        }
+
+        /// <summary>
+        /// Tests if the value is not true.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="error"></param>
+        public static void ShouldNotBeTrue<T>(this T context, String error = null)
+        {
+            var value = Convert.ToBoolean(context);
+            if (error == null) Assert.IsFalse(value);
+            else Assert.IsFalse(value, error);
+        }
+
+        /// <summary>
+        /// Tests if the value contains a string fragment.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="text"></param>
+        /// <param name="error"></param>
+        public static void ShouldContain(this String context, String text, String error = null)
+        {
+            if (error == null) Assert.IsTrue(context.Contains(text));
+            else Assert.IsTrue(context.Contains(text), error);
+        }
+
+        public static void ShouldContain<T>(this IEnumerable<T> context, T item, string error = null)
+        {
+            var isFound = false;
+            foreach (var itm in context)
+            {
+                if (itm.Equals(item))
+                    isFound = true;
+                if (isFound)
+                    break;
+            }
+            Assert.IsTrue(isFound);
+        }
+
+        /// <summary>
+        /// Tests is the value does not contain a string fragment.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="text"></param>
+        /// <param name="error"></param>
+        public static void ShouldNotContain(this String context, String text, String error = null)
+        {
+            if (error == null) Assert.IsFalse(context.Contains(text));
+            else Assert.IsFalse(context.Contains(text), error);
+        }
+    }
+}
---
+++ TestProject1/Testable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace TestProject1
+{
+    public class Testable<T> where T : class
+    {
+        public T Instance { get; set; }
+
+        public Testable() : this(null) { }
+
+        public Testable(params object[] dependancies)
+        {
+            // Inject dependacies for any constructors
+            var ctor = GetConstructorContainingParameters();
+            var objects = CreateInstancesOfConstructorParameters(ctor.GetParameters(), dependancies ?? new object[0]);
+            Instance = (T)ctor.Invoke(objects.ToArray());
+
+            // Inject any dependancies for any property dependancies
+            // Not done yet
+            //var properties = typeof(T).GetProperties(BindingFlags.Public & BindingFlags.NonPublic);
+        }
+
+        private static ConstructorInfo GetConstructorContainingParameters()
+        {
+            // Gets the constructor with the most parameters
+            var ctor = typeof(T).GetConstructors().Where(c => c.GetParameters().Count() > 0)
+                                                  .OrderByDescending(c => c.GetParameters().Count())
+                                                  .FirstOrDefault();
+            return ctor;
+        }
+
+        private static IEnumerable<object> CreateInstancesOfConstructorParameters(ParameterInfo[] parameters, object[] dependancies)
+        {
+            foreach (var param in parameters)
+            {
+                // checks the supplied dependancies array to see if the type was already provided and returns it
+                var dependancy = dependancies.Where(d => d.GetType().GetInterface(param.ParameterType.FullName) != null).FirstOrDefault();
+                if (dependancy != null) { yield return dependancy; continue; }
+
+
+                if (param.ParameterType.IsAbstract | param.ParameterType.IsInterface)
+                {
+                    // Creates a Mock<T> proxy object and adds it to the returning array.
+                    var mockType = typeof(Moq.Mock<>);
+                    var objType = mockType.MakeGenericType(new Type[] { param.ParameterType });
+                    dynamic obj = Activator.CreateInstance(objType);
+                    yield return obj.Object;
+                }
+                else
+                {
+                    // Creates the concrete object and adds it to the returning array.
+                    dynamic obj = Activator.CreateInstance(param.ParameterType);
+                    yield return obj;
+                }
+            }
+        }
+    }
+}
